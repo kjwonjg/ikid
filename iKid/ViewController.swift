309cdc770c5efc171a7fae442a92f8e6b6e6c0a5
Kiwon Jeong <kiwonjg@uw.edu>
2017-10-30 02:33:25 -0700

    test commit

---
+++ iKid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Kiwon Jeong on 2017. 10. 30..
+//  Copyright © 2017년 Kiwon Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
